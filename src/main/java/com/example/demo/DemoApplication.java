dce2d7bd754627adb2ae1275de7292e57284421d
su-mmer <heekim987@gmail.com>
2024-03-13 13:58:43 +0900

    jenkins test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,8 +14,8 @@ public class DemoApplication {
 	}
 
 	@GetMapping(value = "/")
-//	public String doGetHelloWorld() {
-//		return "Hello World";
-//	}
-	public String doGetHelloWorld() { return "This is Two"; }
+	public String doGetHelloWorld() {
+		return "Hello World";
+	}
+//	public String doGetHelloWorld() { return "This is Two"; }
 }
